34339def6b71303f83f5669d08aa0aabb38620dc
nasi <jofce.nasi@gmail.com>
2022-11-10 16:28:11 +0100

    fix: update examples to use node-fetch and be consistent

--- src/1_handling_webhook_events.ts
+++ src/1_handling_webhook_events.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'
+import express, { Request, Response } from 'express'
 import {
   Constants,
   DiagonalError,
@@ -6,14 +8,18 @@ import {
   Webhooks,
 } from '@diagonal-finance/sdk'
 
-import express from 'express'
+dotenv.config()
 
 const app = express()
 
-app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
+app.use(express.json()) // to support JSON-encoded bodies
+app.use(express.urlencoded()) // to support URL-encoded bodies
+
+const endpointSecret = process.env.DIAGONAL_WEBHOOK_ENDPOINT_SECRET as string
+
+app.post('/webhook', (req: Request, res: Response) => {
   let payload = req.body
   let signatureHeader = req.headers[Constants.SIGNATURE_HEADER_KEY] as string
-  const endpointSecret = process.env.DIAGONAL_WEBHOOK_ENDPOINT_SECRET as string
 
   let event: Event
 
@@ -26,7 +32,6 @@ app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
     return res.sendStatus(400)
   }
 
-  // Handle the event
   switch (event.type) {
     case EventType.SIGNATURE_CHARGE_REQUEST:
       console.log(`Charge signature request`)
@@ -39,12 +44,15 @@ app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
       // ...
       break
     default:
+      // Unexpected event type
       console.log(`Unhandled event type ${event.type}.`)
-      break
   }
 
-  // Return a 200 response to acknowledge receipt of the event
+  // Return a 200 response to acknowledge handling of the event
   res.sendStatus(200)
 })
 
-app.listen(3000, () => console.log('Running on port 3000'))
+// start the express server
+app.listen(3000, () => {
+  console.log(`server started at http://localhost:3000`)
+})
